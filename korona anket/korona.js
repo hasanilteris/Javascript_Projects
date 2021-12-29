4bfe2ac5cc06b3cd20cc52ad60886ab8c8de4b09
Hasan İlteriş Dincer <hasanilterisdincer@gmail.com>
2021-12-29 13:49:43 +0300

    Update korona.js

--- korona anket/korona.js
+++ korona anket/korona.js
@@ -4,7 +4,6 @@ const result=document.querySelector('.result');
 
 form.addEventListener('submit',e=>{
     e.preventDefault();
-
     let score=0;
     const userAnswers=[form.q1.value,form.q2.value,form.q3.value,form.q4.value];
 
