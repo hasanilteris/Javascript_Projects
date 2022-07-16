a31c3825042742ffcbdee982e804c0e3135cac35
Hasan İlteriş Dincer <hasanilterisdincer@gmail.com>
2022-07-16 16:45:46 +0300

    Update script.js

--- form validator/script.js
+++ form validator/script.js
@@ -4,6 +4,7 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const repassword = document.getElementById('repassword');
 
+
 function error(input, message) {
     input.className = 'form-control is-invalid';
     const div = input.nextElementSibling;
