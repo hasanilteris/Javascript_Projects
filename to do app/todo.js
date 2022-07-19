31b35a8c9af233a83973c03c57ae3cc3834914c3
Hasan İlteriş Dincer <hasanilterisdincer@gmail.com>
2022-07-19 23:42:23 +0300

    Update todo.js

--- to do app/todo.js
+++ to do app/todo.js
@@ -2,6 +2,7 @@ const addForm=document.querySelector('.add');
 const list=document.querySelector('.todos');
 const search=document.querySelector('.search input');
 const generateTemplate = todo =>{
+    
     const html=`<li class="list-group-item d-flex justify-content-between align-items-center">
     <span>${todo}</span>
     <i class="fas fa-trash-alt delete"></i>
