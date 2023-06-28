4d82109e9b448f0488e973505366348e3b4ab57d
Hasan İlteriş Dincer <hasanilterisdincer@gmail.com>
2023-06-28 03:14:04 +0300

    Update github.js

--- github api/github.js
+++ github api/github.js
@@ -2,7 +2,6 @@ class Github {
     constructor(){
         this.url = "https://api.github.com/users/";
     }
-
     async getGithubData(username){
         const responseUser = await fetch(this.url + username);
         const responseRepo = await fetch(this.url + username + "/repos");
