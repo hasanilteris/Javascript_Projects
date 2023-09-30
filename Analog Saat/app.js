4e3691f63ba78f3f6c4839388f6da2fc755a51c3
Hasan İlteriş Dincer <hasanilterisdincer@gmail.com>
2023-09-30 14:51:25 +0300

    Update app.js

--- Analog Saat/app.js
+++ Analog Saat/app.js
@@ -7,6 +7,7 @@ function tiktak() {
     let second = new Date().getSeconds();
     let minute = new Date().getMinutes();
     let hour = new Date().getHours();
+
     
     sec.style.transform = `translateX(-50%) rotate(${180 + second *  6}deg)`;
     min.style.transform = `translateX(-50%) rotate(${180 + minute *  6}deg)`;
