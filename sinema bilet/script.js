236d9a6bd7216877a5542bf52d6b6a38feff3827
Hasan İlteriş Dincer <hasanilterisdincer@gmail.com>
2023-11-05 22:47:28 +0300

    Update script.js

--- sinema bilet/script.js
+++ sinema bilet/script.js
@@ -3,8 +3,6 @@ const count = document.getElementById('count');
 const amount = document.getElementById('amount');
 const select = document.getElementById('movie');
 const seats = document.querySelectorAll('.seat:not(.reserved)');
-
-
 getFromLocalStorage();
 calculateTotal();
 container.addEventListener('click', function(e) {
