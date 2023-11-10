138d6c6de89f742ffd215eb7be50dd2463d93d63
Hasan İlteriş Dincer <hasanilterisdincer@gmail.com>
2023-11-10 23:26:23 +0300

    Update app.js

--- Countdown Timer/app.js
+++ Countdown Timer/app.js
@@ -8,7 +8,6 @@ function countdown() {
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
     const totalSeconds = (newYearsDate - currentDate) / 1000;
-
     const days = Math.floor(totalSeconds / 3600 / 24);
     const hours = Math.floor(totalSeconds / 3600) % 24;
     const mins = Math.floor(totalSeconds / 60) % 60;
