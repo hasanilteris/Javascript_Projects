c368769f4c351937363cfd5d0ae1f85c414af316
Hasan İlteriş Dincer <hasanilterisdincer@gmail.com>
2023-12-04 17:01:09 +0300

    Update app.js

--- BMI Calculator/app.js
+++ BMI Calculator/app.js
@@ -4,7 +4,6 @@ function CalculateBMI(){
     var result=weight/Math.pow(height,2);
 
     document.getElementById('result').innerText=result.toFixed(2);
-
     if(result<18.5) {
         document.getElementById('result-statu').style.color= '#2196f3';
         document.getElementById('result-statu').innerText='Under Weight';
@@ -26,6 +25,5 @@ function Reset(){
     document.getElementById('weight').value=0;
     document.getElementById('height').value=0;
     document.getElementById('result-statu').innerText='';
-    document.getElementById('result').innerText='';
-    
+    document.getElementById('result').innerText='';  
 }
